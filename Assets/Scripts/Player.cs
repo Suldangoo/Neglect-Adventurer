58699343827a2c15917a8604843301d30092be91
JunHyeong Park <suldangoo@gmail.com>
2023-05-09 02:21:59 +0900

    자동전투 (즉사) 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator playerAnimator;
+    private Animator enemyAnimator;
+
+    float currTime;
+    bool battle = false;
+
     void Start()
     {
-        
+        // 플레이어 오브젝트에서 Animator 컴포넌트 가져오기
+        playerAnimator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if (battle)
+        {
+            currTime += Time.deltaTime;
+            if (currTime >= 0.4f)
+            {
+                playerAnimator.SetBool("Battle", false);
+                currTime = 0;
+                battle = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 충돌한 오브젝트의 태그가 "Monster"인지 확인
+        if (collision.CompareTag("Monster"))
+        {
+            // 적 오브젝트에서 Animator 컴포넌트 가져오기
+            enemyAnimator = collision.GetComponent<Animator>();
+
+            // 플레이어 애니메이터의 Battle 파라미터를 true로 설정
+            playerAnimator.SetBool("Battle", true);
+
+            // 적 애니메이터의 Dead 파라미터를 true로 설정
+            enemyAnimator.SetBool("Dead", true);
+
+            battle = true;
+        }
     }
 }
