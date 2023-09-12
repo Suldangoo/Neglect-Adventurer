528a5bf3752d911622eb9a654cab85c1ecf9f88d
JunHyeong Park <suldangoo@gmail.com>
2023-09-12 18:41:21 +0900

    랭킹 외 SDK 환경 구축

--- Assets/Scripts/Backend/BackendGameData.cs
+++ Assets/Scripts/Backend/BackendGameData.cs
@@ -60,21 +60,140 @@ public class BackendGameData
 
     public void GameDataInsert()
     {
-        // Step 2. 게임정보 삽입 구현하기 
+        if (userData == null)
+        {
+            userData = new UserData();
+        }
+
+        Debug.Log("데이터를 초기화합니다.");
+        userData.level = 1;
+        userData.atk = 3.5f;
+        userData.info = "친추는 언제나 환영입니다.";
+
+        userData.equipment.Add("전사의 투구");
+        userData.equipment.Add("강철 갑옷");
+        userData.equipment.Add("헤르메스의 군화");
+
+        userData.inventory.Add("빨간포션", 1);
+        userData.inventory.Add("하얀포션", 1);
+        userData.inventory.Add("파란포션", 1);
+
+        Debug.Log("뒤끝 업데이트 목록에 해당 데이터들을 추가합니다.");
+        Param param = new Param();
+        param.Add("level", userData.level);
+        param.Add("atk", userData.atk);
+        param.Add("info", userData.info);
+        param.Add("equipment", userData.equipment);
+        param.Add("inventory", userData.inventory);
+
+
+        Debug.Log("게임정보 데이터 삽입을 요청합니다.");
+        var bro = Backend.GameData.Insert("USER_DATA", param);
+
+        if (bro.IsSuccess())
+        {
+            Debug.Log("게임정보 데이터 삽입에 성공했습니다. : " + bro);
+
+            //삽입한 게임정보의 고유값입니다.
+            gameDataRowInDate = bro.GetInDate();
+        }
+        else
+        {
+            Debug.LogError("게임정보 데이터 삽입에 실패했습니다. : " + bro);
+        }
     }
 
     public void GameDataGet()
     {
-        // Step 3. 게임정보 불러오기 구현하기
+        Debug.Log("게임 정보 조회 함수를 호출합니다.");
+        var bro = Backend.GameData.GetMyData("USER_DATA", new Where());
+        if (bro.IsSuccess())
+        {
+            Debug.Log("게임 정보 조회에 성공했습니다. : " + bro);
+
+
+            LitJson.JsonData gameDataJson = bro.FlattenRows(); // Json으로 리턴된 데이터를 받아옵니다.
+
+            // 받아온 데이터의 갯수가 0이라면 데이터가 존재하지 않는 것입니다.
+            if (gameDataJson.Count <= 0)
+            {
+                Debug.LogWarning("데이터가 존재하지 않습니다.");
+            }
+            else
+            {
+                gameDataRowInDate = gameDataJson[0]["inDate"].ToString(); //불러온 게임정보의 고유값입니다.
+
+                userData = new UserData();
+
+                userData.level = int.Parse(gameDataJson[0]["level"].ToString());
+                userData.atk = float.Parse(gameDataJson[0]["atk"].ToString());
+                userData.info = gameDataJson[0]["info"].ToString();
+
+                foreach (string itemKey in gameDataJson[0]["inventory"].Keys)
+                {
+                    userData.inventory.Add(itemKey, int.Parse(gameDataJson[0]["inventory"][itemKey].ToString()));
+                }
+
+                foreach (LitJson.JsonData equip in gameDataJson[0]["equipment"])
+                {
+                    userData.equipment.Add(equip.ToString());
+                }
+
+                Debug.Log(userData.ToString());
+            }
+        }
+        else
+        {
+            Debug.LogError("게임 정보 조회에 실패했습니다. : " + bro);
+        }
     }
 
     public void LevelUp()
     {
-        // Step 4. 게임정보 수정 구현하기
+        Debug.Log("레벨을 1 증가시킵니다.");
+        userData.level += 1;
+        userData.atk += 3.5f;
+        userData.info = "내용을 변경합니다.";
     }
 
+    // 게임정보 수정하기
     public void GameDataUpdate()
     {
-        // Step 4. 게임정보 수정 구현하기
+        if (userData == null)
+        {
+            Debug.LogError("서버에서 다운받거나 새로 삽입한 데이터가 존재하지 않습니다. Insert 혹은 Get을 통해 데이터를 생성해주세요.");
+            return;
+        }
+
+        Param param = new Param();
+        param.Add("level", userData.level);
+        param.Add("atk", userData.atk);
+        param.Add("info", userData.info);
+        param.Add("equipment", userData.equipment);
+        param.Add("inventory", userData.inventory);
+
+        BackendReturnObject bro = null;
+
+        if (string.IsNullOrEmpty(gameDataRowInDate))
+        {
+            Debug.Log("내 제일 최신 게임정보 데이터 수정을 요청합니다.");
+
+            bro = Backend.GameData.Update("USER_DATA", new Where(), param);
+        }
+        else
+        {
+            Debug.Log($"{gameDataRowInDate}의 게임정보 데이터 수정을 요청합니다.");
+
+            bro = Backend.GameData.UpdateV2("USER_DATA", gameDataRowInDate, Backend.UserInDate, param);
+        }
+
+        if (bro.IsSuccess())
+        {
+            Debug.Log("게임정보 데이터 수정에 성공했습니다. : " + bro);
+        }
+        else
+        {
+            Debug.LogError("게임정보 데이터 수정에 실패했습니다. : " + bro);
+        }
     }
 }
--- Assets/Scripts/Backend/BackendManager.cs
+++ Assets/Scripts/Backend/BackendManager.cs
@@ -23,13 +23,22 @@ public class BackendManager : MonoBehaviour
         Test();
     }
 
-    // 동기 함수를 비동기에서 호출하게 해주는 함수(유니티 UI 접근 불가)
     async void Test()
     {
         await Task.Run(() => {
             BackendLogin.Instance.CustomLogin("user1", "1234"); // 뒤끝 로그인
 
-            // 게임 정보 기능 구현 로직 추가
+            BackendGameData.Instance.GameDataGet(); // 데이터 삽입 함수
+
+            // [추가] 서버에 불러온 데이터가 존재하지 않을 경우, 데이터를 새로 생성하여 삽입
+            if (BackendGameData.userData == null)
+            {
+                BackendGameData.Instance.GameDataInsert();
+            }
+
+            BackendGameData.Instance.LevelUp(); // [추가] 로컬에 저장된 데이터를 변경
+
+            BackendGameData.Instance.GameDataUpdate(); //[추가] 서버에 저장된 데이터를 덮어쓰기(변경된 부분만)
 
             Debug.Log("테스트를 종료합니다.");
         });
