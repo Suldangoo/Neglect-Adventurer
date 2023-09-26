ead8e54ca035015b7504fdbc5eb4f08ebf8aacdc
JunHyeong Park <suldangoo@gmail.com>
2023-09-26 20:58:18 +0900

    스크립트 리

--- Assets/Scripts/Backend/GameDataViewer.cs
+++ Assets/Scripts/Backend/GameDataViewer.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using TMPro;
 
-public class TopPanelViewer : MonoBehaviour
+public class GameDataViewer : MonoBehaviour
 {
 	[SerializeField]
 	private	TextMeshProUGUI	textNickname;
