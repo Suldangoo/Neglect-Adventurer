714d0eecb024543f95058c0fb390fe6f3139f1f3
JunHyeong Park <suldangoo@gmail.com>
2023-09-26 21:12:36 +0900

    서버 데이터 불러오기 성공 (전투레벨)

--- Assets/Scripts/Backend/GameDataViewer.cs
+++ Assets/Scripts/Backend/GameDataViewer.cs
@@ -5,11 +5,25 @@ public class GameDataViewer : MonoBehaviour
 {
 	[SerializeField]
 	private	TextMeshProUGUI	textNickname;
+	[SerializeField]
+	private TextMeshProUGUI textLevel;
+	//[SerializeField]
+	//private TextMeshProUGUI textGold;
 
+	private void Awake()
+	{
+		BackendGameData.Instance.onGameDataLoadEvent.AddListener(UpdateGameData);
+	}
 	public void UpdateNickname()
 	{
 		// 닉네임이 없으면 gamer_id를 출력하고, 닉네임이 있으면 닉네임 출력
 		textNickname.text = UserInfo.Data.nickname == null ?
 							UserInfo.Data.gamerId : UserInfo.Data.nickname;
+	}
+
+	public void UpdateGameData()
+	{
+		textLevel.text = $"{BackendGameData.Instance.UserGameData.level}";
+		// textGold.text = $"{BackendGameData.Instance.UserGameData.gold}";
 	}
 }
