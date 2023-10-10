e90b820e9c55559660939c3c3217a2ef20de4599
JunHyeong Park <suldangoo@gmail.com>
2023-10-10 20:53:08 +0900

    DailyRankRegister 작성중

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,5 @@
+public static class Constants
+{
+	public static readonly string	USER_DATA_TABLE	= "USER_DATA";
+	public static readonly string	GOLD_RANK_UUID	= "5c2d5ef0-6761-11ee-bf02-d9e6da20488f";
+}
