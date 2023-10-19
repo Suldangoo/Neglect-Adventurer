e9833de55f4f304c1aecdaf6bf31dc71174f66a2
JunHyeong Park <suldangoo@gmail.com>
2023-10-20 01:49:23 +0900

    코드 최적화

--- Assets/Scripts/Guild.cs
+++ Assets/Scripts/Guild.cs
@@ -30,9 +30,29 @@ public class Guild : MonoBehaviour
     public Image[] prameImages;
     public TextMeshProUGUI[] resultTexts; // 캐릭터 이름
 
+    // 각 등급의 색상
+    private readonly Color32 colorGrade1 = new Color32(255, 255, 255, 255); // FFFFFF
+    private readonly Color32 colorGrade2 = new Color32(153, 255, 153, 255); // 99FF99
+    private readonly Color32 colorGrade3 = new Color32(255, 249, 141, 255); // FFF98D
 
-    // 뽑은 캐릭터의 이름과 등급을 출력할 TextMeshPro 컴포넌트
+    // 뽑기 설정 구조체
+    struct GachaSetting
+    {
+        public int minIndex;
+        public int maxIndex;
+        public int grade1;
+        public int grade2;
+        public float grade1Chance;
 
+        public GachaSetting(int min, int max, int g1, int g2, float chance)
+        {
+            minIndex = min;
+            maxIndex = max;
+            grade1 = g1;
+            grade2 = g2;
+            grade1Chance = chance;
+        }
+    }
 
     private void Awake()
     {
@@ -48,70 +68,68 @@ public class Guild : MonoBehaviour
 
     public void OnClickNomalOnePick()
     {
-        int minIndex = 0, maxIndex = 6, grade1 = 1, grade2 = 2; // 1~2성 범위 설정
-        float grade1Chance = 0.9f;
-        PerformPick(minIndex, maxIndex, grade1, grade2, grade1Chance);
+        GachaSetting setting = new GachaSetting(0, 6, 1, 2, 0.9f);
+        PerformPick(setting, 1);
     }
 
     public void OnClickRareOnePick()
     {
-        int minIndex = 3, maxIndex = 9, grade1 = 2, grade2 = 3; // 2~3성 범위 설정
-        float grade1Chance = 0.9f;
-        PerformPick(minIndex, maxIndex, grade1, grade2, grade1Chance);
-    }
-
-    private void PerformPick(int minIndex, int maxIndex, int grade1, int grade2, float grade1Chance)
-    {
-        (Sprite selectedSprite, int grade, int index) = PickCharacterInRange(minIndex, maxIndex, grade1, grade2, grade1Chance);
-        resultImage.sprite = selectedSprite;
-        SetFrame(grade);
-        string starRepresentation = new string('★', grade);
-        resultText.text = $"{starRepresentation} {characterNames[index]}";
+        GachaSetting setting = new GachaSetting(3, 9, 2, 3, 0.9f);
+        PerformPick(setting, 1);
     }
 
     public void OnClickNomalTenPick()
     {
-        int minIndex = 0, maxIndex = 6, grade1 = 1, grade2 = 2; // 1~2성 범위 설정
-        float grade1Chance = 0.9f;
-        PerformTenPick(minIndex, maxIndex, grade1, grade2, grade1Chance);
+        GachaSetting setting = new GachaSetting(0, 6, 1, 2, 0.9f);
+        PerformPick(setting, 10);
     }
 
     public void OnClickRareTenPick()
     {
-        int minIndex = 3, maxIndex = 9, grade1 = 2, grade2 = 3; // 2~3성 범위 설정
-        float grade1Chance = 0.9f;
-        PerformTenPick(minIndex, maxIndex, grade1, grade2, grade1Chance);
+        GachaSetting setting = new GachaSetting(3, 9, 2, 3, 0.9f);
+        PerformPick(setting, 10);
     }
 
-    private void PerformTenPick(int minIndex, int maxIndex, int grade1, int grade2, float grade1Chance)
+    private void PerformPick(GachaSetting setting, int times)
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < times; i++)
         {
-            (Sprite selectedSprite, int grade, int index) = PickCharacterInRange(minIndex, maxIndex, grade1, grade2, grade1Chance);
-            resultImages[i].sprite = selectedSprite;
-            prameImages[i].sprite = prameSprites[grade - 1]; // 프레임 설정
+            (Sprite selectedSprite, int grade, int index) = PickCharacterInRange(setting);
 
-            string starRepresentation = new string('★', grade);
-            resultTexts[i].text = $"{starRepresentation} {characterNames[index]}"; // 캐릭터 이름 출력
+            if (times == 1)
+            {
+                resultImage.sprite = selectedSprite;
+                resultText.text = FormatCharacterText(grade, index);
+                SetTextColorBasedOnGrade(resultText, grade);
+                prameImage.sprite = prameSprites[grade - 1];
+            }
+            else
+            {
+                resultImages[i].sprite = selectedSprite;
+                resultTexts[i].text = FormatCharacterText(grade, index);
+                SetTextColorBasedOnGrade(resultTexts[i], grade);
+                prameImages[i].sprite = prameSprites[grade - 1];
+            }
         }
     }
 
-    private (Sprite, int, int) PickCharacterInRange(int minIndex, int maxIndex, int grade1, int grade2, float grade1Chance)
+    private (Sprite, int, int) PickCharacterInRange(GachaSetting setting)
     {
         float randomValue = Random.value;
         int selectedIndex;
 
-        if (randomValue <= grade1Chance)
+        if (randomValue <= setting.grade1Chance)
         {
-            selectedIndex = Random.Range(minIndex, minIndex + 3);
-            return (characterSprites[selectedIndex], grade1, selectedIndex);
+            selectedIndex = Random.Range(setting.minIndex, setting.minIndex + 3);
+            return (characterSprites[selectedIndex], setting.grade1, selectedIndex);
         }
         else
         {
-            selectedIndex = Random.Range(minIndex + 3, maxIndex);
-            return (characterSprites[selectedIndex], grade2, selectedIndex);
+            selectedIndex = Random.Range(setting.minIndex + 3, setting.maxIndex);
+            return (characterSprites[selectedIndex], setting.grade2, selectedIndex);
         }
     }
+
 
     private void SetFrame(int grade)
     {
@@ -125,6 +143,31 @@ public class Guild : MonoBehaviour
                 break;
             case 3:
                 prameImage.sprite = prameSprites[2]; // 3성 프레임
+                break;
+            default:
+                Debug.LogWarning($"Unexpected grade: {grade}");
+                break;
+        }
+    }
+
+    private string FormatCharacterText(int grade, int index)
+    {
+        string starRepresentation = new string('★', grade);
+        return $"{starRepresentation} {characterNames[index]}";
+    }
+
+    private void SetTextColorBasedOnGrade(TextMeshProUGUI textComponent, int grade)
+    {
+        switch (grade)
+        {
+            case 1:
+                textComponent.color = colorGrade1;
+                break;
+            case 2:
+                textComponent.color = colorGrade2;
+                break;
+            case 3:
+                textComponent.color = colorGrade3;
                 break;
             default:
                 Debug.LogWarning($"Unexpected grade: {grade}");
