5248d3b4caaf7c9cd7e19202694cbf4cc4634c69
suldangoo <suldangoo@gmail.com>
2023-10-26 18:54:57 +0900

    우편 시스템 구축

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,4 +3,6 @@ public static class Constants
 	public static readonly string	USER_DATA_TABLE	= "USER_DATA";
 	public static readonly string	GOLD_RANK_UUID	= "5c2d5ef0-6761-11ee-bf02-d9e6da20488f";
 	public static readonly int		MAX_RANK_LIST	= 20;
+
+	public static readonly string GOODS_CHART_NAME = "재화차트";
 }
